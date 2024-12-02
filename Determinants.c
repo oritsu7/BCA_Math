e27bba88c71fb2447036118461cf6135e33831ef
oritsu7 <oritsuminamoto@gmail.com>
2024-12-02 12:19:12 +0530

    added Cramer's rule function

--- Determinants.c
+++ Determinants.c
@@ -4,11 +4,17 @@
 int main(){
     int num1 = 2; // order 2x2
     int num2 = 3; // order 3x3
+    int a, b, c;
     int mat1[2][2] = {{4,6}, {2,5}};
     int mat2[3][3] = {{2, 0, 5}, {3 ,1, 1}, {1, 4, 6}};
+    int mat3[3][3] = {{3, 5, -7}, {4, 1, -12}, {2, 9, -3}};
+    int constant[3] = {13, 6, 20};
 
     int x = determinant(num1, mat1);
     int y = determinant(num2, mat2);
     printf("%d\n", x);
-    printf("%d", y);
+    printf("%d\n", y);
+
+    cramers_rule(&a, &b, &c, mat3, constant);
+    printf("A: %d B: %d C: %d", a, b, c);
 }
--- bca_math.h
+++ bca_math.h
@@ -1,5 +1,5 @@
 // Function to calculate the determinant of a matrix
-int determinant(int n, int mat[n][n]) {
+int determinant(int n, int mat[n][n]) { // ? is there an another way to do this? (mat[n][n])
     if (n == 2) {
         return (mat[0][0] * mat[1][1]) - (mat[0][1] * mat[1][0]);
     }
@@ -7,7 +7,7 @@ int determinant(int n, int mat[n][n]) {
     int det = 0;
     for (int col = 0; col < n; col++) {
         // Create a minor matrix
-        int minor[n-1][n-1];
+        int minor[n-1][n-1]; // ! expression must have a constant value problem
         for (int i = 1; i < n; i++) { // Skip the first row
             int minor_col = 0;
             for (int j = 0; j < n; j++) {
@@ -22,4 +22,40 @@ int determinant(int n, int mat[n][n]) {
         det += cofactor * determinant(n-1, minor);
     }
     return det;
+}
+
+void cramers_rule(int* x, int* y, int* z, int mat[3][3], int *constant){
+    int det = determinant(3, mat);
+    if(det == 0)
+        return;   
+    int mat_x[3][3]; 
+    int mat_y[3][3];
+    int mat_z[3][3];
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            mat_x[i][j] = mat[i][j];
+            mat_y[i][j] = mat[i][j];
+            mat_z[i][j] = mat[i][j];
+        }
+    }
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            if(i==0)
+                mat_x[j][i] = constant[j];
+            else if(i == 1)
+                mat_y[j][i] = constant[j];
+            else
+                mat_z[j][i] = constant[j];             
+        }
+    }
+
+    int det_x = determinant(3, mat_x);
+    int det_y = determinant(3, mat_y);
+    int det_z = determinant(3, mat_z);
+
+    *x = det_x/det;
+    *y = det_y/det;
+    *z = det_z/det;
 }
